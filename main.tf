82d4401c90bc9148a197b957cfd6be0900ad87b2
Pedro <pedrofcox360@outlook.com>
2025-04-12 22:14:13 -0600

    Se agrega VPC y configs basicas

---
+++ main.tf
@@ -0,0 +1,76 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# VPC
+resource "aws_vpc" "vpc_act3_pfdls" {
+  cidr_block           = "10.10.0.0/20"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "act3-vpc-pfdls"
+  }
+}
+
+# Subred pública
+resource "aws_subnet" "subred_publica_act3_pfdls" {
+  vpc_id                  = aws_vpc.vpc_act3_pfdls.id
+  cidr_block              = "10.10.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "act3-subred-publica-pfdls"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw_act3_pfdls" {
+  vpc_id = aws_vpc.vpc_act3_pfdls.id
+  tags = {
+    Name = "act3-gateway-pfdls"
+  }
+}
+
+# Tabla de rutas
+resource "aws_route_table" "rt_publica_act3_pfdls" {
+  vpc_id = aws_vpc.vpc_act3_pfdls.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_act3_pfdls.id
+  }
+
+  tags = {
+    Name = "act3-rt-publica-pfdls"
+  }
+}
+
+# Asociación de tabla de rutas
+resource "aws_route_table_association" "asociacion_rt_act3_pfdls" {
+  subnet_id      = aws_subnet.subred_publica_act3_pfdls.id
+  route_table_id = aws_route_table.rt_publica_act3_pfdls.id
+}
+# Security Group para Jump Server
+resource "aws_security_group" "sg_jump_act3_pfdls" {
+  name        = "act3-sg-jump-pfdls"
+  description = "Permitir SSH desde internet - PFDLS"
+  vpc_id      = aws_vpc.vpc_act3_pfdls.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "act3-sg-jump-pfdls"
+  }
+}
